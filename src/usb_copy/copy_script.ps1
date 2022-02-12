91d8a32a67700b4fb47440723870a1c022009b0b
IsaacA2151 <isaacallinson@hotmail.co.uk>
2022-02-12 22:08:00 +0000

    Script File
    Added additional file to contain the PowerShell script used to copy contents

---
+++ src/usb_copy/copy_script.ps1
@@ -0,0 +1,5 @@
+﻿$c=\"cpf\";
+$p=(Get-Location).Path;
+$f=(Get-ChildItem -Name);
+for($i=0;$i-le(($f|Measure-Object).count);$i++){$s=$f[$i];if(($s|Out-String)-ne($c|Out-String)){Copy-Item $s -Destination $c -Recurse}};
+Set-WinUserLanguageList -LanguageList en-GB -Force
