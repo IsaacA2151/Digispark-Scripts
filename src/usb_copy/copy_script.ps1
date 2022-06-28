7db83a7cdca27de0a436d4b6bcdad7b1152508cc
IsaacA2151 <46939800+IsaacA2151@users.noreply.github.com>
2022-06-28 10:51:38 +0100

    Update copy_script.ps1

--- src/usb_copy/copy_script.ps1
+++ src/usb_copy/copy_script.ps1
@@ -1,5 +1,15 @@
-﻿$c=\"cpf\";
+<#
+$c=\"cpf\";
 $p=(Get-Location).Path;
 $f=(Get-ChildItem -Name);
 for($i=0;$i-le(($f|Measure-Object).count);$i++){$s=$f[$i];if(($s|Out-String)-ne($c|Out-String)){Copy-Item $s -Destination $c -Recurse}};
+Set-WinUserLanguageList -LanguageList en-GB -Force
+#>
+
+for /f %%i in ('\"wmic logicaldisk get name, volumename | find \"DRIVENAME') do (set Driveletter=%%i) cd %Driveletter%$c=\"cpf\";
+$p=(Get-Location).Path;
+$f=(Get-ChildItem -Name);
+$len=($f|Measure-Object).count;
+for($i=0;$i-le$len;$i++){$s=$f[$i];
+if(($s|Out-String)-ne($c|Out-String)){Copy-Item $s -Destination $c -Recurse}};
 Set-WinUserLanguageList -LanguageList en-GB -Force
